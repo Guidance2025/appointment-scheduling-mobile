07e1280a48ad589ecb314b47c367582f926f91a7
NNeonael <neonael.angcaya.nazareno@gmail.com>
2026-01-18 16:31:31 +0800

    update-v4

--- constants/api.js
+++ constants/api.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = "http:/10.10.2.188:8080";
+export const API_BASE_URL = "http://192.168.1.15:8080";
 
 export const REGISTER_FCM_TOKEN = `${API_BASE_URL}/notification/registerToken`;
 
@@ -7,3 +7,4 @@ export const USER_LOGIN_ENDPOINT = `${API_BASE_URL}/user/login`;
 
 export const GET_NOTIFICATION_BY_USER = (userId) => (`${API_BASE_URL}/notification/${userId}`);
 
+    
