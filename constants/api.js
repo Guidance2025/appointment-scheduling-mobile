e1973e87a19cc03f3f98e2f0883a90c7e2da025e
NNeonael <neonael.angcaya.nazareno@gmail.com>
2026-01-21 21:48:33 +0800

    fix ci pipeline

--- constants/api.js
+++ constants/api.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 export const API_BASE_URL = "http://192.168.1.15:8080";
-=======
-export const API_BASE_URL = "http://172.20.10.3:8080";
->>>>>>> 84b6e908fc69f6eb5d26aeeeaf70d2acdd3efe76
 
 export const REGISTER_FCM_TOKEN = `${API_BASE_URL}/notification/registerToken`;
 
