4104f11b9a14f7fe784666519f2445929e992377
NNeonael <neonael.angcaya.nazareno@gmail.com>
2026-01-27 12:17:19 +0800

    fix

--- constants/api.js
+++ constants/api.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = "http://192.168.1.15:8080";
+export const API_BASE_URL = "http://10.16.76.34:8080";
 
 export const REGISTER_FCM_TOKEN = `${API_BASE_URL}/notification/registerToken`;
 
